e25f7dc0d3415bd37320a2cf92915c4e0c4b3366
Marco Mina <marco.mina.85@gmail.com>
2011-08-03 14:16:29 +0200

    First insertion

---
+++ src/GO/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+"""
+Module for parsing and management of GOs and GOAs.
+"""
+__author__="Marco Mina"
+__email__="marco.mina.85@gmail.com"
+
+__all__ = ["AnnotationCorpus", "GeneOntology"]
---
+++ src/SemSim/avgSemSim.py
@@ -0,0 +1,50 @@
+# -*- coding: iso-8859-1 -*-
+"""
+This class provides the prototype for a generic mixing strategy for pairwise Terms Protein Semantic Similarity measures (mixSS)
+"""
+
+#from GO import AnnotationCorpus
+#from GO import GeneOntology
+#from SemSimUtils import *
+from MixSemSim import *
+import sys
+import os
+import math
+
+class avgSemSim(MixSemSim):
+
+	def int_SemSim(self, scores):
+		if len(scores) == 0:
+			return 0
+		finale = 0
+		for i in scores:
+			#print(scores[i]
+			if i[2] == -1:
+				print("Errore in avgSemSim")
+				sys.exit(1)
+			finale += i[2]
+		finale /= float(len(scores))
+		return finale
+
+#if __name__ == "__main__":
+	##### load annotations
+	#gp = AnnotationCorpus.AnnotationCorpus()
+	#gp.parse(sys.argv[2])
+
+	##### load ontology
+	#tree = GeneOntology.get_go_graph(open(sys.argv[1]))
+	#print("Ontology infos: file name: " + str(sys.argv[1]) + ". Nodes: " + str(tree.V.__len__()) + ". Edges: " + str(tree.E.__len__())
+	
+	##### create SemSimUtils class
+	#ssu = SemSimUtils(gp, tree)
+	#ssu.det_offspring_table()
+	#ssu.det_ancestors_table()
+	#ssu.det_freq_table()
+	#ssu.det_GO_division()
+	#ssu.det_ICs_table()
+
+	#TSS = TermSemSim(gp, tree, ssu)
+	#test = TSS.SemSim("GO:0030682","GO:0030683")
+	#print(test
+	#test = TSS.SemSim("GO:0008150","GO:0008150")
+	#print(test
