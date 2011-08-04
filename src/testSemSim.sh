15dfce06a363a825a172bcfd6124b4e21071cd47
Marco Mina <marco.mina.85@gmail.com>
2011-08-04 22:59:54 +0200

    version 0.1 stable

--- src/testSemSim.sh
+++ src/testSemSim.sh
@@ -1,1 +1,1 @@
-python2 testSemSim.py /mnt/dati/SS/src/test/GO_2011-06-06.obo-xml  /mnt/dati/SS/src/test/human /mnt/dati/SS/src/test/human_list.txt  > out.txt
+(export PYTHONPATH="."; python2 testSemSim.py ../../GO/GO_2011-06-06.obo-xml  ../../GOA/human ../../src/test/human_list.txt  > out.txt)
