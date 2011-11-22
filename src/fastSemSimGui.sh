0824b9b9fac5e543381466cb4c0c33d513ff20b7
Marco Mina <marco.mina.85@gmail.com>
2011-11-22 13:13:21 -0500

    ok# Please enter the commit message for your changes. Lines starting
    Former-commit-id: d9232e0ab4afab2595a43537d39fdfa1b82c7d2b

--- src/fastSemSimGui.sh
+++ src/fastSemSimGui.sh
@@ -1,1 +1,14 @@
-(export PYTHONPATH="."; python2 gui/fastSemSimGui.py)
+#!/bin/bash
+
+# set LOCAL to 0 to run the installed version of the GUI
+LOCAL=1
+
+PYTHON_INTERPRETER=python
+
+if [ ${LOCAL} = 1 ]; then
+	echo "Running the local version"
+	(export PYTHONPATH="."; ${PYTHON_INTERPRETER} gui/fastSemSimGui.py)
+else
+	echo "Running the installed version"
+	(cd examples && ${PYTHON_INTERPRETER} LoadGUI.py)
+fi
