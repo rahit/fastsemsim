9c4b2891b09dfc25887b0a43cbdb426cd47e69e3
Marco Mina <marco.mina.85@gmail.com>
2012-02-24 11:21:23 -0500

    cool# Please enter the commit message for your changes. Lines starting
    Former-commit-id: c326485d586a53732458f9ee5e27a1e823cd0f6c

--- setup/install.sh
+++ setup/install.sh
@@ -19,16 +19,8 @@
 # along with fastSemSim.  If not, see <http://www.gnu.org/licenses/>.
 # '''
 
-# set LOCAL to 0 to run the installed version of the GUI
-# LOCAL=1
-
 PYTHON_INTERPRETER=python
 
-if [ -f startfastSemSim.py ]; then
-# if [ ${LOCAL} = 1 ]; then
-	echo "Running the local version"
-	(export PYTHONPATH="."; ${PYTHON_INTERPRETER} startfastSemSim.py "$@")
-else
-# 	echo "Running the installed version"
-	${PYTHON_INTERPRETER} -c "from fastSemSim import startfastSemSim; startfastSemSim.start()" "$@"
-fi
+python setup.py install
+cp -i -v fastSemSim.sh /usr/bin/fastSemSim
+cp -i -v fastSemSimGui.sh /usr/bin/fastSemSimGui
